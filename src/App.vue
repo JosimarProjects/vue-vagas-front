91fad3b5b71ed63b87f8a7b7295218d3ba2793ec
JosimarProjects <jo030191@gmail.com>
2022-06-12 14:32:55 -0300

    adicionando header

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,26 @@
 <template>
-  <Topo></Topo>
-  <Conteudo></Conteudo>
+  <topo-padrao></topo-padrao>
+  <conteudo></conteudo>
 </template>
 
 <script>
 
 
 import Conteudo from './components/layouts/Conteudo.vue'
-import Topo from './components/layouts/Topo.vue'
+import TopoPadrao from './components/layouts/TopoPadrao.vue'
 
 
 export default {
   name: 'App',
   components: {
     Conteudo,
-    Topo
+   TopoPadrao
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
