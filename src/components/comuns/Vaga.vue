8048645ef160919e16873cd2a0ae20bdf43f880f
JosimarProjects <jo030191@gmail.com>
2022-06-14 12:29:03 -0300

    avancando

---
+++ src/components/comuns/Vaga.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card">
+    <div class="card-header bg-dark text-white"> {{ titulo }}</div>
+    <div class="card-body">
+      <p>{{ descricao }}</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Salário:R$6.000,00 | Modalidade: Home Office | Tipo: CLT| Data:10/11/2021 </small>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Vaga',
+  props: [
+   "titulo",
+    "descricao",
+    "salario",
+    "modalidade",
+    "tipo",
+    "publicacao"
+  ]
+}
+</script>
